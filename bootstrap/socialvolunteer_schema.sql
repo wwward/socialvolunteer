9db9aadfa65fa8118e73da903e4fd522889147d0
miria <mirialeigh@gmail.com>
2013-03-31 16:40:51 -0400

    Added additional fields based on the data model

--- bootstrap/socialvolunteer_schema.sql
+++ bootstrap/socialvolunteer_schema.sql
@@ -2,12 +2,11 @@
 -- www3 - Mar 26, 0:20
 CREATE TABLE Volunteer(id VARCHAR(30), name VARCHAR(60), phone
 VARCHAR(15), location VARCHAR(60), friends VARCHAR(30), total_score
-SMALLINT, reputation SMALLINT);
-CREATE TABLE Organization(id VARCHAR(30), name VARCHAR(60), phone
-VARCHAR(15), location VARCHAR(60), reputation SMALLINT);
+SMALLINT, reputation SMALLINT, username VARCHAR(30));
+CREATE TABLE Organization(id VARCHAR(30), name VARCHAR(60), phone VARCHAR(15), location VARCHAR(60), reputation SMALLINT, description VARCHAR(100));
 CREATE TABLE Job(id VARCHAR(30), organization_id VARCHAR(30), event_date
-DATE, score_value SMALLINT, committed VARCHAR(30), completed
-VARCHAR(30),description VARCHAR(255));
+DATE, event_time SMALLINT, event_day_of_week SMALLINT, score_value SMALLINT, committed VARCHAR(30), completed
+VARCHAR(30), description VARCHAR(255), category VARCHAR(255));
 CREATE TABLE Keyword(keyword VARCHAR(30), reference_id VARCHAR(30));
 -- Two simple indexes to shorten lookup times
 CREATE INDEX Idx_volunteer ON Volunteer (location);

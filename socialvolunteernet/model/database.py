86b0c4fdc5377ea6a918253245ce06ed35b3f5c2
miria <mirialeigh@gmail.com>
2013-03-31 19:29:14 -0400

    Added db changes

---
+++ socialvolunteernet/model/database.py
@@ -0,0 +1,39 @@
+from google.appengine.api import rdbms
+
+
+class GoogleCloudSQLStore(object):
+
+    def __init__(self, params):
+        INSTANCE_NAME="groupwerk"
+        DATABASE="volunteerdb"
+        self._conn = rdbms.connect(instance=INSTANCE_NAME, database=DATABASE)
+        
+    def select(self, sql, params=None):
+        cursor = self._conn.cursor()
+        cursor.execute(sql, params)
+
+        # Convert from a list of lists to a list of maps where the key is the column name       
+        rows = []
+        column_names = self.get_columns(cursor)
+        for row in cursor.fetchall():
+            result = {}
+            for i in range(0, len(row)):
+                result[column_names[i]] = row[i]
+            rows.append(result)
+        cursor.close()
+        return rows
+
+    # Extract the column names from the cursor    
+    def get_columns(self, cursor):
+        names = []
+        for (name, _,_,_,_,_,_) in cursor.description:
+            names.append(name)
+        return names
+    
+    def update(self, sql, params=None):
+        cursor = self._conn.cursor()
+        cursor.execute(sql,params)
+        self._conn.commit()
+        cursor.close()
+
+    
--- socialvolunteernet/model/organization.py
+++ socialvolunteernet/model/organization.py
@@ -1,17 +1,22 @@
-'''
-Created on Mar 29, 2013
-
-@author: miria
-'''
+from socialvolunteernet.model.database import GoogleCloudSQLStore
 
 class Organization(object):
-    '''
-    classdocs
-    '''
-
 
     def __init__(self, params):
-        '''
-        Constructor
-        '''
+        self.db = GoogleCloudSQLStore()
+    
+    CREATE_ORGANIZATION = """
+        INSERT INTO Organization VALUES 
+        (%(id)s, %(name)s, %(phone)s, %(location)s, 0, %(description)s )
+    """
+    def create_new(self, **kw):
+        for key in ('id', 'name', 'phone', 'location', 'description'):
+            if not key in kw:
+                print 'Cannot create organization - missing field %s' % key
+                return False
+        self.db.update(self.CREATE_ORGANIZATION, kw)
+        return True;
+    
+    
+
         

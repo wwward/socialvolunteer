3f9b208eb0fa55dc2868ed268d27b1c462dc83b3
William Ward <wwward@pobox.com>
2013-04-28 18:59:06 -0400

    Added SQL to all the Organizational function calls.

--- socialvolunteernet/model/organization.py
+++ socialvolunteernet/model/organization.py
@@ -19,40 +19,103 @@ class Organization(object):
         return True;
 
     #Returns the total count of all jobs completed through the site for this organization
+    # Status (0 = cancelled, 1 = incomplete, 2 = complete)
+    GET_COMPLETED_JOB_COUNT = """
+        select count(status) from groupwerk.Job 
+        where organization_id = %(organization_id)s and status = 1
+    """
     def get_completed_job_count(self, organization_id):
-        pass
+        return self.db.select(self.GET_COMPLETED_JOB_COUNT, 
+                              {"organization_id": organization_id})
     
     #Returns all currently open job listings
+    GET_CURRENT_JOBS = """
+        select Job.id, Job.description
+        from Job 
+        WHERE Job.organization_id = %(organization_id)s
+    """
     def get_current_jobs(self, organization_id):
-        pass
+        return self.db.select(self.GET_CURRENT_JOBS, 
+                              {"organization_id": organization_id})
     
-    # Returns all jobs that users have commited to, along with user_ids, maybe some user data
+    # Returns all jobs that users have committed to, along with user_ids, maybe some user data
+    GET_COMMITTED_JOBS = """
+        select Job.id, Job.description, Volunteer.id 
+        from Job,Volunteer,Job_volunteer 
+        WHERE Job.id = Job_volunteer.job_id 
+        AND Job_volunteer.volunteer_id = Volunteer.id
+        AND Job.organization_id = %(organization_id)s
+    """
     def get_committed_jobs(self, organization_id):
-        pass
+        return self.db.select(self.GET_COMMITTED_JOBS, 
+                              {"organization_id": organization_id})
     
     # Returns all jobs that users have completed but have yet to be reviewed
+    # Status 4 = reviewed
+    GET_UNREVIEWED_JOBS = """
+        select count(status) from groupwerk.Job 
+        where organization_id = %(organization_id)s and status = 4
+    """
     def get_unreviewed_jobs(self, organization_id):
-        pass
+        return self.db.select(self.GET_UNREVIEWED_JOBS, 
+                              {"organization_id": organization_id})
     
     # Returns all jobs that are completed and reviewed
+    # Completed + Reviewed status = 5
+    GET_COMPLETED_JOBS = """
+        select * from groupwerk.Job 
+        where organization_id = %(organization_id)s and status = 5
+    """
     def get_completed_jobs(self, organization_id):
-        pass
+        return self.db.select(self.GET_COMPLETED_JOBS, 
+                              {"organization_id": organization_id})
     
     # Get info about a given organization id
+    GET_INFO = """
+        select * from groupwerk.Organization 
+        where id = %(organization_id)s
+    """
     def get_info(self, organization_id):
-        pass
+        return self.db.select(self.GET_INFO,
+                              {"organization_id": organization_id})
     
     # Delete a listed job
+    DELETE_JOB = """
+        delete from Job where organization_id = %(organization_id)s 
+        AND job_id = %(job_id)s
+    """
     def delete_job(self, organization_id, job_id):
-        pass
+        return self.db.select(self.DELETE_JOB,
+                              {"organization_id": organization_id, "job_id": job_id})
     
     # Edit a listed job. Modified fields are in the kw dictionary
+    EDIT_JOB = """
+        UPDATE Job SET id=%(id)s, organization_id=%(organization_id)s, 
+                       event_date=%(event_date)s, event_time=%(event_time)s,
+                       event_duration_minutes=%(event_duration_minutes)s,
+                       score_value=%(score_value)s, description=%(description)s,
+                       category=%(category)s, status=%(status)s 
+                       WHERE id=%(job_id)s AND organization_id=%(organization_id)s
+    """
     def edit_job(self, organization_id, job_id, **kw):
-        pass
+        self.db.update(self.EDIT_JOB, kw)
     
     # Add a new job. The fields are in the kw dict
+    ADD_JOB = """
+       INSERT INTO Job VALUES (id=%(id)s, organization_id=%(organization_id)s, 
+                       event_date=%(event_date)s, event_time=%(event_time)s,
+                       event_duration_minutes=%(event_duration_minutes)s,
+                       score_value=%(score_value)s, description=%(description)s,
+                       category=%(category)s, status=%(status)s)
+    """
     def add_job(self, **kw):
-        pass
-    
+        self.db.update(self.ADD_JOB, kw)
+        
+    # Edit organization
+    EDIT_ORGANIZATION = """
+        UPDATE Organization SET id=%(id)s, name=%(name)s, phone=%(phone)s, location=%(location)s,
+                                reputation=%(reputation)s, description=%(description)s
+                                WHERE id=%(organization_id)s
+    """
     def edit_organization_data(self, organization_id, **kw):
-        pass
+        self.db.update(self.EDIT_ORGANIZATION, kw)
